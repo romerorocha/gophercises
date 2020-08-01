c66e6b8ea235b9021869cb153c94400c2a8f04ea
Claudio Romero <romerolrocha@gmail.com>
2020-08-01 18:06:20 -0300

    Solve 'URL Shortener'

---
+++ urlshort/handler.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYAML(yaml)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func parseYAML(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
